7fbc80a100e4650a6217056d58493a4f629cbb04
OlenaKashuba <amely.jazz@gmail.com>
2018-03-06 12:11:13 +0000

    Form submit

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,47 @@
 import React from 'react';
 
 class Search extends React.Component{
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			value: ''
+		}
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	handleChange(event) {
+		this.setState({
+			value: event.target.value
+		});
+	}
+
+	handleSubmit(event) {
+		console.log(this.state);
+		event.preventDefault();
+		alert('submit clicked');
+	}
+
+
 	render(){
 		return(
-			<form className='search-form'>
+			<form 	
+				className='search-form'
+				onSubmit={this.handleSubmit}
+			>
 				<label htmlFor='search-input'>Search By Name</label>
 				<input 
+					onChange={this.handleChange}
+					value={this.state.value}
 					type='text' 
 					id='search-input'
 					className='search-input'
 					name='search-input' 
 				/>
-				<button className='submit-btn'>Find</button>
+				<button className='submit-btn' type='submit'>
+					Find
+				</button>
 			</form>
 		);
 	}
